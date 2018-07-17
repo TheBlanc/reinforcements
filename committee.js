b554dfd53752bfedd57cc9c42f4ce53bc9501f7e
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-07-17 17:11:44 -0400

    completed reinforcement

---
+++ committee.js
@@ -0,0 +1,53 @@
+var project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  dueDate : "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     dueDate: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     dueDate: "January 1, 2018"
+    },
+    {description: "compile results",
+     dueDate: "November 10, 2018"
+    },
+    {description: "version 1",
+     dueDate: "January 15, 2019"
+    },
+    {description: "revisions",
+     dueDate: "March 30, 2019"
+    },
+    {description: "version 2",
+     dueDate: "July 12, 2019"
+    },
+    {description: "final edit",
+     dueDate: "October 1, 2019"
+    },
+    {description: "final version",
+     dueDate: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     dueDate: "December 1, 2019"
+    }
+  ]
+}
+
+var i = 0;
+project.steps = project.steps.map( function(step) {
+
+  if (i > 2) { i = 0; }
+  step = {
+    description: step.description,
+    dueDate: step.dueDate,
+    person: project.committee[i],
+
+  }
+
+  i++;
+  return step;
+
+});
+
+console.log(project);
